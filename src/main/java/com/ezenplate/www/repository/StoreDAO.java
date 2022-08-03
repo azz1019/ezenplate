7a245ecfbc1d28e25a85ac1b0bec6d4c5b707915
X2X2XE <58972100+X2X2XE@users.noreply.github.com>
2022-08-03 19:56:36 +0900

    Update StoreDAO.java

--- src/main/java/com/ezenplate/www/repository/StoreDAO.java
+++ src/main/java/com/ezenplate/www/repository/StoreDAO.java
@@ -12,4 +12,12 @@ public interface StoreDAO {
 	int remove(long sno); // 식당 삭제
 	int selectTotalCount(PagingVO pgvo); // 전체 식당 갯수 반환
 	long selectLastSno(); // 가장 최근등록한 식당의 sno 반환
+	
+	/* 맛집 목록 및 검색*/
+	List<StoreVO> select_store_list(PagingVO pgvo); // 전체 맛집 목록
+	List<StoreVO> select_search_store(PagingVO pgvo); // 검색한 맛집
+	int select_total_count(PagingVO pgvo); // 맛집 전체 수
+	int select_search_count(PagingVO pgvo); // 검색한 맛집 수
+	void update_readcount(long sno, int i); // 맛집 조회수 증가
+	StoreVO select_one(long sno); // 맛집 detail
 }
