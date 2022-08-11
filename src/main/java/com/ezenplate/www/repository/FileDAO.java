cebb93206aa4005dd4721e6accace326f4c0926b
X2X2XE <58972100+X2X2XE@users.noreply.github.com>
2022-08-11 16:32:35 +0900

    Update FileDAO.java

--- src/main/java/com/ezenplate/www/repository/FileDAO.java
+++ src/main/java/com/ezenplate/www/repository/FileDAO.java
@@ -25,4 +25,23 @@ public interface FileDAO {
 
 	public int deleteFile(String uuid); // 파일 삭제
 	public List<FileVO> selectAllFiles(); // 전체 파일 목록
+	
+	/* 맛집 목록 및 검색*/
+	int insertFile(FileVO fvo);
+	int insert_user_th(FileVO file);
+	List<FileVO> selectListFile(long no);
+
+	int deleteAllFile(long no);
+	
+	FileVO selectFile(String email);
+	List<FileVO> select_store_file(long sno); // 검색한 맛집 이미지 파일 가져오기
+	List<FileVO> select_store_all_file(long sno); // 전체 맛집 이미지 파일 가져오기
+	List<FileVO> select_review(long rno); // 맛집 리뷰 이미지 파일 가져오기
+	int delete_review_File(long rno); // 맛집 리뷰 이미지 삭제 시 
+	List<FileVO> selectBoardListFile(long bno);
+//	int deleteFile(String uuid); // 중복
+//	List<FileVO> selectAllFiles(); // 중복
+//	int deleteBoardFile(long bno);  //중복
+//	int insertBoardFile(FileVO fvo); //중복
+	int insert_review_file(FileVO fvo); // 리뷰 이미지 추가
 }
