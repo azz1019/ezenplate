507b10015910539b8c55aa26378a888285abfdf6
O3Olivia <cherishtmwu@gmail.com>
2022-08-11 16:53:02 +0900

    comment serviceimpl 수정

--- src/main/java/com/ezenplate/www/service/CommentServiceImpl.java
+++ src/main/java/com/ezenplate/www/service/CommentServiceImpl.java
@@ -3,6 +3,7 @@ package com.ezenplate.www.service;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ezenplate.www.domain.CommentVO;
 import com.ezenplate.www.domain.PagingVO;
@@ -10,7 +11,9 @@ import com.ezenplate.www.handler.PagingHandler;
 import com.ezenplate.www.repository.BoardDAO;
 import com.ezenplate.www.repository.CommentDAO;
 
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class CommentServiceImpl implements CommentService {
 
@@ -37,12 +40,19 @@ public class CommentServiceImpl implements CommentService {
 
 	@Override
 	public int modify(CommentVO cvo) {
-		return 0;
+		return cdao.update(cvo);
 	}
 
+	@Transactional
 	@Override
 	public int remove(long cno) {
-		return 0;
+		long bno = cdao.selectBno(cno);
+		int isDown = cdao.delete(cno);
+		if(isDown > 0) {
+			isDown = bdao.updateCmtQty(bno, -1);
+			log.info(">>> Comment remove -get : {}", isDown > 0? "OK":"FAIL");
+		}
+		return isDown;
 	}
 
 }
