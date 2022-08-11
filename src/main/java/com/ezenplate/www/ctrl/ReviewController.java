f343c7f889fcb9033bf5585df710404ef6297fc1
X2X2XE <58972100+X2X2XE@users.noreply.github.com>
2022-08-11 21:31:32 +0900

    Update ReviewController.java

--- src/main/java/com/ezenplate/www/ctrl/ReviewController.java
+++ src/main/java/com/ezenplate/www/ctrl/ReviewController.java
@@ -1,19 +1,25 @@
 package com.ezenplate.www.ctrl;
 
-import java.util.List;
+mport java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.POST;
 
 import org.apache.cxf.jaxrs.ext.multipart.Multipart;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ezenplate.www.domain.FileVO;
 import com.ezenplate.www.domain.PagingVO;
