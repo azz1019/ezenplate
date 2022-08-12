04dfd7e39863bf62faf30e4a58d6140f7b2da5bf
X2X2XE <58972100+X2X2XE@users.noreply.github.com>
2022-08-12 21:41:58 +0900

    Update WantService.java

--- src/main/java/com/ezenplate/www/service/WantService.java
+++ src/main/java/com/ezenplate/www/service/WantService.java
@@ -9,4 +9,6 @@ public interface WantService {
 	public List<WantVO> getList(PagingVO pgvo, long mno); // 가고싶다 목록
 	public int remove(long sno, long mno); // 가고싶다 삭제
 	public int getTotalCount(PagingVO pgvo, long mno); // 가고싶다 갯수 반환
+	public int check(long sno, long mno); // 중복확인
+	public int register(long sno, long mno); // 가고싶다 생성
 }
