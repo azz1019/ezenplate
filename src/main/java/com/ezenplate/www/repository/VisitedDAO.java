aade2e7965ff2263a28428db9f8aff44c32c4da5
X2X2XE <58972100+X2X2XE@users.noreply.github.com>
2022-08-12 21:01:26 +0900

    Update VisitedDAO.java

--- src/main/java/com/ezenplate/www/repository/VisitedDAO.java
+++ src/main/java/com/ezenplate/www/repository/VisitedDAO.java
@@ -3,6 +3,8 @@ package com.ezenplate.www.repository;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.ezenplate.www.domain.VisitedVO;
 
 public interface VisitedDAO {
@@ -10,4 +12,6 @@ public interface VisitedDAO {
 	int remove(Map map); // 가봤어요 삭제
 	int selectTotalCount(Map map); // 가봤어요 갯수 반환
 	int removeAllList(long mno); // 탈퇴시 가봤어요 목록 삭제
+	int register(@Param("mno") long mno,@Param("sno") long sno,@Param("sname") String sname);
+	int check(VisitedVO vvo);
 }
