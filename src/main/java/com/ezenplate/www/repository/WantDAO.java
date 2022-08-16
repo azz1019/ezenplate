1112a7c4c3b9042abbdb926eac958b58dcc93e51
X2X2XE <58972100+X2X2XE@users.noreply.github.com>
2022-08-16 14:58:32 +0900

    Update WantDAO.java

--- src/main/java/com/ezenplate/www/repository/WantDAO.java
+++ src/main/java/com/ezenplate/www/repository/WantDAO.java
@@ -12,4 +12,5 @@ public interface WantDAO {
 	int removeAllList(long mno); // 탈퇴시 가고싶다 목록 삭제
 	int check_dul(WantVO wvo); // 중복확인
 	int insert_want(WantVO wvo); // 가고싶다 추가
+	List<WantVO> want_get_list(long mno); // 회원 가고싶다 리스트 가져
 }
