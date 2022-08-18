1a00992d6007bcdc86ff5a5a53b1893219886a3b
mansekim <102591845+mansekim@users.noreply.github.com>
2022-08-18 21:25:19 +0900

    Update store.remove.js

--- src/main/webapp/resources/js/store.remove.js
+++ src/main/webapp/resources/js/store.remove.js
@@ -1,9 +1,7 @@
 document.getElementById('storeRemove').addEventListener('click', (e) => {
-    console.log("왜안돼");
     e.preventDefault();
 
     let snoVal = document.getElementById('snoVal').innerText;
-    console.log(snoVal);
     document.getElementById('sno').value = snoVal;
     const delForm = document.getElementById('storeRemoveForm');
     delForm.setAttribute('action', '/store/remove');
