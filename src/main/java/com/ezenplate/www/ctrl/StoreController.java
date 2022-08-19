2323eb0db0fd60e5bdf9ea9cab74171cad96e597
mansekim <102591845+mansekim@users.noreply.github.com>
2022-08-19 19:29:51 +0900

    Update StoreController.java

--- src/main/java/com/ezenplate/www/ctrl/StoreController.java
+++ src/main/java/com/ezenplate/www/ctrl/StoreController.java
@@ -38,8 +38,8 @@ public class StoreController {
 	public void approve(Model model, PagingVO pgvo) {
 		log.info(">>> StoreController approve - GET");
 		model.addAttribute("list", ssv.getList(pgvo));
-		int totalCount = ssv.getTotalCount(pgvo);
-		model.addAttribute("pgn", new PagingHandler(pgvo, totalCount));
+		int totalApproveCount = ssv.getTotalApproveCount(pgvo);
+		model.addAttribute("pgn", new PagingHandler(pgvo, totalApproveCount));
 	}
 	
 	@GetMapping("/admit")
