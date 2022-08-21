1f8ef4c83a6a7aee651a7970f00dab66cdf90184
X2X2XE <58972100+X2X2XE@users.noreply.github.com>
2022-08-21 17:19:21 +0900

    Create store.remove.js

--- src/main/webapp/resources/js/store.remove.js
+++ src/main/webapp/resources/js/store.remove.js
@@ -2,7 +2,7 @@ document.getElementById('storeRemove').addEventListener('click', (e) => {
     e.preventDefault();
 
     let snoVal = document.getElementById('snoVal').innerText;
-    document.getElementById('sno').value = snoVal;
+    document.getElementById('sno_1').value = snoVal;
     const delForm = document.getElementById('storeRemoveForm');
     delForm.setAttribute('action', '/store/remove');
     delForm.submit();
