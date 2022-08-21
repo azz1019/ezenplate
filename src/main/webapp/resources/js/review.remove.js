2b8fd537a467e3c040a169d492472b7355351a1d
X2X2XE <58972100+X2X2XE@users.noreply.github.com>
2022-08-21 17:33:53 +0900

    Create review.remove.js

--- src/main/webapp/resources/js/review.remove.js
+++ src/main/webapp/resources/js/review.remove.js
@@ -2,7 +2,7 @@ document.getElementById('reviewRemove').addEventListener('click', (e) => {
     e.preventDefault();
 
     let rnoVal = document.getElementById('rnoVal').innerText;
-    document.getElementById('rno').value = rnoVal;
+    document.getElementById('rno_1').value = rnoVal;
     const delForm = document.getElementById('reviewRemoveForm');
     delForm.setAttribute('action', '/review/reportremove');
     delForm.submit();
